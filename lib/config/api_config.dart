9f3e5a0fd0d3f6cf758632ad757648bf55e3b3aa
adalbertomina <amina@mercantilbanco.com.pa>
2025-05-07 21:03:33 -0500

    Ajustes

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,6 +1,6 @@
 // Definir una constante global para la URL de la API
 const String API_BASE_URL = String.fromEnvironment('API_BASE_URL',
-    defaultValue: 'http://t0gsggssgg8ow04ocwww08oo.195.35.36.123.sslip.io');
+    defaultValue: 'https://t0gsggssgg8ow04ocwww08oo.195.35.36.123.sslip.io');
 // URL forzada para todos los entornos
 const String FORCED_API_URL = API_BASE_URL;
 
